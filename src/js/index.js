d1e271e4e8be5fa984fe24099b03b2f238f4a30a
kruisher <kruisher@gmail.com>
2025-01-31 17:32:34 +0100

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+const firstField = document.querySelector("#firstNumber");
+const secondField = document.querySelector("#secondNumber");
+const actionSelect = document.querySelector("#action");
+const equalButton = document.querySelector("#equal");
+
+equalButton.addEventListener("click", () => {
+    const firstNumber = parseFloat(firstField.value);
+    const secondNumber = parseFloat(secondField.value);
+    const action = actionSelect.value;
+    let result;
+
+    switch (action) {
+        case "+":
+            result = firstNumber + secondNumber;
+            break;
+        case "-":
+            result = firstNumber - secondNumber;
+            break;
+        case "*":
+            result = firstNumber * secondNumber;
+            break;
+        case "/":
+            if(secondNumber === 0) {
+                alert("На нуль ділити не можна")
+                return;
+            }
+            result = firstNumber / secondNumber;
+    }
+})
+
